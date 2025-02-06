dcb3fa78f269e2f8408d20aeede9e577a7552a89
MayarMenessySK <mayarmenasy15@gmail.com>
2025-02-06 12:16:25 +0200

    Q1

---
+++ Assignments/Assignment1/src/app/Question1/q1/q1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-q1',
+  standalone: false,
+  templateUrl: './q1.component.html',
+  styleUrl: './q1.component.scss'
+})
+export class Q1Component {
+  username: string = '';
+}
--- Assignments/Assignment1/src/app/app.module.ts
+++ Assignments/Assignment1/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Q1Component } from './Question1/q1/q1.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Q1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
