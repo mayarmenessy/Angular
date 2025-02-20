b6868dab7539f19d34d29871e472629aa69c8bfd
MayarMenessySK <mayar.menessy@dilverkeytech.com>
2025-02-20 19:42:11 +0200

    makeing Child and Parent Components

--- Lectures/Lec3/src/app/server-element/server-element.component.ts
+++ Lectures/Lec3/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './server-element.component.scss'
 })
 export class ServerElementComponent {
-
+@Input() element:{type:string, name:string, content:string};
 }
