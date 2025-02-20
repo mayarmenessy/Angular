5c8af502dbddffbcec5f327f03c9244f408050af
MayarMenessySK <mayar.menessy@dilverkeytech.com>
2025-02-20 16:56:27 +0200

    Adding Lec3

--- Lectures/Lec3/src/app/app.module.ts
+++ Lectures/Lec3/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
---
+++ Lectures/Lec3/src/app/server-element/server-element.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  standalone: false,
+  templateUrl: './server-element.component.html',
+  styleUrl: './server-element.component.scss'
+})
+export class ServerElementComponent {
+
+}
